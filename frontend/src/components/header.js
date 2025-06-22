4bbae451f199b6ab7b88bb08ce877598322fa458
Natalya Aragão <33945558+natalyaaragao@users.noreply.github.com>
2025-06-22 20:40:31 -0300

    Update header.js

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,15 +1,35 @@
 // src/components/Header.js
-import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.css';
+import './header.css'; // você pode criar esse CSS separado ou usar inline
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">
-        <Link to="/lobby">Adedonha Online</Link>
+      <div className="header-content">
+        <div className="logo-section">
+          <Link to="/"><h1 className="logo-text">Adedonha Online</h1></Link>
+        </div>
+        <nav className="nav">
+          <Link to="/lobby">
+            <div className="nav-button">
+                <span className="nav-icon">👤</span>
+                Lobby
+            </div>
+          </Link>
+          <Link to="/perfil">
+            <div className="nav-button">
+              <span className="nav-icon">👤</span>
+              Perfil
+            </div>
+          </Link>
+          <Link to="/">
+            <div className="nav-button nav-button-outline">
+              <span className="nav-icon">🚪</span>
+              Login
+            </div>
+          </Link>
+        </nav>
       </div>
-
     </header>
   );
 };
